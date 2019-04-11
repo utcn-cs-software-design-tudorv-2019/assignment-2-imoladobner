6d91721d5340c146cdd5c1f6725e5d5166757695
Imola Dobner <imola.dobner@yahoo.com>
2019-04-11 13:01:15 +0300

    first

--- Assignment2/src/main/java/proj/a2/model/repository/CouseDAO.java
+++
@@ -1,9 +0,0 @@
-package proj.a2.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import proj.a2.model.entity.Course;
-
-public interface CouseDAO extends JpaRepository<Course, Integer> {
-
-}
--- Assignment2/src/main/java/proj/a2/model/repository/EnrollmentDAO.java
+++
@@ -1,9 +0,0 @@
-package proj.a2.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import proj.a2.model.entity.Enrollment;
-
-public interface EnrollmentDAO extends JpaRepository<Enrollment, Integer>{
-
-}
